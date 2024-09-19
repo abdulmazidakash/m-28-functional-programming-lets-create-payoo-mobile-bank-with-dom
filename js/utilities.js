6d8e4a25ca77264313d650afb15da97e2904b190
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-19 22:40:30 +0600

    28-3 recap add money layout handler and reasons for shared code

---
+++ js/utilities.js
@@ -0,0 +1,12 @@
+// console.log('utilities file added');
+
+
+/**
+ * common shared functions here
+ */
+
+console.log('utilities loaded');
+
+function getInputFieldValueById(){
+	 console.log('will get value by id');
+}
