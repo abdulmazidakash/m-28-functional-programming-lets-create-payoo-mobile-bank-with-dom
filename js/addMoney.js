755c28da85204bab6d836f46949e52736d84b967
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-19 22:16:12 +0600

    28-2 recap add money and new files for function based payoo

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,8 +2,21 @@
 document.getElementById("btn-add").addEventListener('click', function(event){
 	event.preventDefault();
 
-	const addMoney = document.getElementById('btn-add-money').value ;
-	const pinNumber = document.getElementById('btn-pin-number').value;
+	const addMoney = document.getElementById('input-add-money').value ;
+	const pinNumber = document.getElementById('input-pin-number').value;
 
-	console.log(addMoney, pinNumber);
+	// console.log(addMoney, pinNumber);
+
+	if (pinNumber === '1234'){
+
+		const balance = document.getElementById('account-balance').innerText;
+		const balanceNumber = parseFloat(balance);
+		const addMoneyNumber = parseFloat(addMoney);
+
+		const newBalance = balanceNumber + addMoneyNumber;
+
+		document.getElementById('account-balance').innerText = newBalance;
+	}else{
+		alert('failed password , please try again later');
+	}
 })
