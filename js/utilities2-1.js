3c7f2f68e921b37288ff4072a8e85921a5d5f9c6
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-20 11:44:32 +0600

    28-5 use shared function to get input value

---
+++ js/utilities2-1.js
@@ -0,0 +1,8 @@
+function getInputFieldValueById(id){
+	const inputValue = document.getElementById(id).value ;
+	const inputNumber = parseFloat(inputValue);
+	
+	console.log(id, inputValue, inputNumber);
+	
+	return inputNumber;
+}
