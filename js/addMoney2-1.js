2ffac09c8d045e6ac3e69dc79fa7b924717cd8c0
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-20 11:45:07 +0600

    readded

--- js/addMoney2-1.js
+++ js/addMoney2-1.js
@@ -4,5 +4,15 @@ document.getElementById('btn-add-money')
 
 	const addMoney = getInputFieldValueById('input-add-money');
 	const pinNumber = getInputFieldValueById('input-pin-number')
-	console.log('add money inside addMoney2.js', addMoney, pinNumber);
+
+
+	// console.log('add money inside addMoney2.js', addMoney, pinNumber);
+	//
+	//wrong way to verify . do not try it at your serious website
+
+	if(pinNumber === 1234){
+		
+	}else{
+		alert('failed add the money.')
+	}
 })
