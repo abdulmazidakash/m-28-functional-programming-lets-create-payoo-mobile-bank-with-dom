49ff7448750f869afb0b086c323661462c55491d
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-20 14:44:58 +0600

    28-8 show transactions

--- js/addMoney2-1.js
+++ js/addMoney2-1.js
@@ -13,10 +13,18 @@ document.getElementById('btn-add-money')
 	if(pinNumber === 1234){
 
 		const balance = getTextFieldValueById("account-balance");
-		console.log(balance, addMoney);
+		// console.log(balance, addMoney);
 		const newBalance = balance + addMoney;
 
 		document.getElementById('account-balance').innerText = newBalance;
+
+		//add to transaction history
+		const p = document.createElement('p');
+		p.innerText = `Added: ${addMoney} Tk. New Balance: ${newBalance}`;
+		console.log(p);
+
+		//should be a common function 
+		document.getElementById('transaction-container').appendChild(p);
 	}else{
 		alert('failed add the money.')
 	}
